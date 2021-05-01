0ec5bf9fade5ddd284392c5af1f6764fa6034fb2
yn1323 <yn1323@gmail.com>
2021-05-01 21:40:07 +0900

    fix

--- src/component/molecule/AnimalList.tsx
+++ src/component/molecule/AnimalList.tsx
@@ -11,18 +11,22 @@ const AnimalList = ({ callback, selected }: Props) => {
   return (
     <IonGrid>
       <IonRow>
-        {images.map(({ index, path }, key) => (
-          <IonCol
-            key={key}
-            size="2"
-            className={`cursor-pointer align-centerVH ${
-              selected === index ? 'is-selected' : ''
-            }`}
-            onClick={() => callback(index)}
-          >
-            <img src={path} />
-          </IonCol>
-        ))}
+        {images.map(({ index, path }, key) =>
+          index !== '99' ? (
+            <IonCol
+              key={key}
+              size="2"
+              className={`cursor-pointer align-centerVH ${
+                selected === index ? 'is-selected' : ''
+              }`}
+              onClick={() => callback(index)}
+            >
+              <img src={path} />
+            </IonCol>
+          ) : (
+            <div key={key}></div>
+          )
+        )}
       </IonRow>
     </IonGrid>
   )
