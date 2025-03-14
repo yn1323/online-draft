d1e6f12f1d864a5133f1ee6200c9594f115e372b
y-natani <yn1323@gmail.com>
2025-03-15 00:07:37 +0900

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -21,15 +21,19 @@ function MyApp({ Component, pageProps }: AppProps) {
         <Head>
           <title>オンラインドラフト会議</title>
         </Head>
-        <Script id="gtm" strategy="beforeInteractive">
-          {`
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','${GTM_ID}');
-        `}
-        </Script>
+        <Script
+          id="gtm"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${GTM_ID}');
+              `,
+          }}
+        />
         <Component {...pageProps} />
       </Provider>
     </ChakraProvider>
