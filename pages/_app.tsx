ac579dc8bab192a7a856f332731e675ee057760d
y-natani <yn1323@gmail.com>
2025-03-14 23:53:14 +0900

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
+import Script from 'next/script'
 import React from 'react'
 import { Provider } from 'react-redux'
 import { themeChakra } from '@/constants/theme'
@@ -12,9 +13,8 @@ import '@/styles/page/Draft.scss'
 import '@/styles/page/Loading.scss'
 import '@/styles/page/New.scss'
 
-
 function MyApp({ Component, pageProps }: AppProps) {
-  const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID || '';
+  const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID || ''
   return (
     <ChakraProvider theme={themeChakra}>
       <Provider store={store}>
@@ -22,14 +22,14 @@ function MyApp({ Component, pageProps }: AppProps) {
           <title>オンラインドラフト会議</title>
         </Head>
         <Script id="gtm" strategy="beforeInteractive">
-        {`
+          {`
           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','${GTM_ID}');
         `}
-      </Script>
+        </Script>
         <Component {...pageProps} />
       </Provider>
     </ChakraProvider>
